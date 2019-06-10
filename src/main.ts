7b630962e07d629ac6ff992e57942e8d6bb11c06
陈小祥 <chenxiaoxiang@CXX-MacMini.local>
2019-06-10 21:40:08 +0800

    :sparkles: 拦截器+守卫

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filers/http-exception.filter';
 import { ApiParamsValidationPipe } from './common/pipes/api-params-validation.pipe';
 import { logger } from './common/middleware/logger.middleware';
+import { AuthGuard } from './common/guards/auth.guard';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
   app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new AuthGuard());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ApiParamsValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(8080);
 }
 bootstrap();
